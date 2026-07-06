b1b8f5bc339e249fc5d83e3e9f1ab7fd1472587a
GuilhermeSZB <szaboguilherme21@gmail.com>
2026-07-06 19:34:01 -0300

    Contagem Regressiva

---
+++ Python/contagem-regressiva.py
@@ -0,0 +1,11 @@
+print (" ==== BEM-VINDO ==== ");
+print("Este é o sistema de contagem regressiva");
+print(" ");
+
+b = 10
+
+for i in range(10):
+    print(b)
+    b = b - 1
+
+print ("Decolar!!!")
