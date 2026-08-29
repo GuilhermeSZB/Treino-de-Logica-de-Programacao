9fd6e5b26ca93e09818e05a187eccde4d99dbc79
GuilhermeSZB <szaboguilherme21@gmail.com>
2026-08-29 00:26:34 -0300

    Aprendendo a usar o parametro String

--- Java/relembrando.java
+++ Java/relembrando.java
@@ -7,8 +7,8 @@ public class relembrando {
     static double num1;
     static double num2;
 
-    private static double nums(){
-        System.out.print("Qual é o numero: ");
+    private static double nums(String texto){
+        System.out.print(texto);
         return numero = sc.nextDouble();
 
     }
@@ -18,8 +18,8 @@ public class relembrando {
     }
 
     public static void main(String[] args) {
-        num1 = nums();
-        num2 = nums();
+        num1 = nums("Qual é o primerio numero: ");
+        num2 = nums("Qual é o segundo numero: ");
         double res = soma(num1, num2);
 
         System.out.println("O resultado é de " + res);
